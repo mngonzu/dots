2f78c168700f36f3802dd00d40944b7f44afcb23
Manuel Gonzalez <68573545+mngonzu@users.noreply.github.com>
2023-11-16 17:27:40 -0300

    Added CoC

--- init.lua
+++ init.lua
@@ -12,5 +12,4 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 require("lazy").setup("plugins")
-require("config/config")
-
+vim.cmd.colorscheme "catppuccin-latte"
--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -1,1 +1,29 @@
-return { "catppuccin/nvim", name = "catppuccin", priority = 1000 }
+return { "catppuccin/nvim", 
+	  name = "catppuccin", 
+	  priority = 1000, 
+	  config = function()
+		require("catppuccin").setup(
+		{flavour = latte,
+		     integrations = {coc_nvim = true,
+		     		     native_lsp = {
+    enabled = true,
+    virtual_text = {
+        errors = { "italic" },
+        hints = { "italic" },
+        warnings = { "italic" },
+        information = { "italic" },
+    },
+    underlines = {
+        errors = { "underline" },
+        hints = { "underline" },
+        warnings = { "underline" },
+        information = { "underline" },
+    },
+    inlay_hints = {
+        background = true,
+    },
+},
+		     		    }
+		})
+	end,
+}
